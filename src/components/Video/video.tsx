f940ee96f52493493c8e584ff151e2906128d85c
Shark919 <christoph.meise@gmx.de>
2021-03-13 12:53:47 +0100

    Fix embed

--- src/components/Video/video.tsx
+++ src/components/Video/video.tsx
@@ -1,6 +1,6 @@
 import { Trans } from 'gatsby-plugin-react-i18next';
 import React from 'react';
-import { Container, Embed, Grid, GridColumn, Header } from 'semantic-ui-react';
+import { Container, Grid, GridColumn, Header } from 'semantic-ui-react';
 import './video.less';
 
 const Video = ({ }) => {
@@ -9,16 +9,18 @@ const Video = ({ }) => {
             <Container>
                 <Grid columns="2" stackable centered>
                     <GridColumn width="10">
-                        <div>
-                            <Embed
-                                id="zy1MzATrJkc"
-                                aspectRatio="16:9"
-                                className="shadow rounded"
-                                placeholder="images/hqdefault.jpg"
-                                alt="youtube-image-placeholder"
-                                source="youtube"
-                                autoplay
-                            />
+                        <div className="embed-wrap rounded shadow">
+                            <iframe
+                                width="100%"
+                                height="100%"
+                                src="https://www.youtube.com/embed/zy1MzATrJkc"
+                                className="yt-iframe-embed"
+                                srcDoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/zy1MzATrJkc?autoplay=1><img src=images/hqdefault.jpg alt='WeWater Team Video'><span>▶</span></a>"
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                                title="WeWater Team Video"
+                            ></iframe>
                         </div>
                     </GridColumn>
                     <GridColumn width="6">
