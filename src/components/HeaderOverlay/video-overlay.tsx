36a6ef34d63425092d2c57c7fc6a5e043631ca3f
Shark919 <christoph.meise@gmx.de>
2021-04-01 15:25:15 +0200

    Fix ul bug, fix muted

--- src/components/HeaderOverlay/video-overlay.tsx
+++ src/components/HeaderOverlay/video-overlay.tsx
@@ -15,33 +15,7 @@ const VideoOverlay = ({ sourceMP4, sourceWebm, sourceOGV, content, darken = fals
         // check if user agent is safari and we have the ref to the container <div />
         if (videoParentRef.current) {
             // obtain reference to the video element
-            const player = videoParentRef.current.children[0];
-
-            // if the reference to video player has been obtained
-            if (player) {
-                // set the video attributes using javascript as per the
-                // webkit Policy
-                player.controls = false;
-                player.playsinline = true;
-                player.muted = true;
-                player.setAttribute("muted", ""); // leave no stones unturned :)
-                player.autoplay = true;
-
-                // Let's wait for an event loop tick and be async.
-                setTimeout(() => {
-                    // player.play() might return a promise but it's not guaranteed crossbrowser.
-                    const promise = player.play();
-                    // let's play safe to ensure that if we do have a promise
-                    if (promise.then) {
-                        promise
-                            .then(() => { })
-                            .catch(() => {
-                                // if promise fails, hide the video and fallback to <img> tag
-                                videoParentRef.current.style.display = "none";
-                            });
-                    }
-                }, 0);
-            }
+            videoParentRef.current.setAttribute("muted", "");
         }
     }, []);
 
