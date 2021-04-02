5559e8c63ab5460da9fb83d8b523a0be754137c1
Shark919 <christoph.meise@gmx.de>
2021-04-02 09:39:38 +0200

    Fix caching issue

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,5 @@ export { wrapRootElement } from './src/apollo/wrap-root-element';
 export const shouldUpdateScroll = () => {
   document.body.scrollTop = 0;
 };
+
+export const onServiceWorkerUpdateReady = () => window.location.reload();
