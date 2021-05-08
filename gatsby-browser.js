942c4dc3fd2f8f01a63efc6f6577d3daa4dc1b4b
Shark919 <christoph.meise@gmx.de>
2021-05-08 12:30:52 +0200

    Fix service worker remainigns

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,13 +5,3 @@ export { wrapRootElement } from './src/apollo/wrap-root-element';
 export const shouldUpdateScroll = () => {
   document.body.scrollTop = 0;
 };
-
-export const onRouteUpdate = () => {
-  navigator.serviceWorker.register('/sw.js').then((reg) => {
-    reg.update();
-  });
-};
-
-export const onServiceWorkerUpdateReady = () => {
-  window.location.reload(true)
-};
