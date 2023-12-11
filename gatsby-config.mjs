3ef0eeba3c3ab4b0b7a337ccbb12d4bb6a0331f8
christophmeise <christoph.meise@gmx.de>
2023-12-11 14:57:38 +0100

    adapter

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import * as adapter from "gatsby-adapter-netlify";
+import pkg from "gatsby-adapter-netlify";
+const { adapter } = pkg;
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
