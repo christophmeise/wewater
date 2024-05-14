764f89ecf8a1887361c083542bb6d740e52d7b92
christophmeise <christoph.meise@gmx.de>
2024-05-14 19:11:35 +0200

    new tracking id

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -80,7 +80,7 @@ const config = {
       resolve: `gatsby-plugin-gdpr-cookies`,
       options: {
         googleAnalytics: {
-          trackingId: "376959100", // leave empty if you want to disable the tracker
+          trackingId: "G-H8Z3B7N6GM", // leave empty if you want to disable the tracker
           cookieName: "gatsby-gdpr-google-analytics", // default
           anonymize: true, // default
           allowAdFeatures: false, // default
